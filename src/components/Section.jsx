8f947e4a2a673c610c07887d292412336f1c8fe8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-17 23:23:02 +0300

    create files

---
+++ src/components/Section.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class Toggle extends Component {
+  state = { isOpen: false };
+
+  toggle = () => {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const { children } = this.props;
+
+    return (
+      <div>
+        <button onClick={this.toggle}>{isOpen ? "Hide" : "Show"}</button>
+        {isOpen && children}
+      </div>
+    );
+  }
+}
+
+/*
+FriendList.propTypes = {
+    friends: propTypes.arrayOf(
+        propTypes.shape(
+            {
+                avatar: propTypes.string.isRequired,
+                name: propTypes.string.isRequired,
+                isOnline: propTypes.bool.isRequired,
+                id: propTypes.number.isRequired,
+            }
+        )
+    ).isRequired,
+}
+*/
