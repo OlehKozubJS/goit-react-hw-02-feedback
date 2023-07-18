775a4c804a742c38b14981e180a43d1a964b9568
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 01:50:36 +0300

    Update Statistics.jsx

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -1,17 +1,17 @@
 import { Component } from "react";
 
 export class Statistics extends Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
+    render() {
+        const { good, neutral, bad, total, positivePercentage } = this.props;
 
-    return (
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {positivePercentage}%</li>
-      </ul>
-    );
-  }
+        return (
+            <ul>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+                <li>Total: {total}</li>
+                <li>Positive feedback: {positivePercentage}%</li>
+            </ul>
+        );
+    }
 }
