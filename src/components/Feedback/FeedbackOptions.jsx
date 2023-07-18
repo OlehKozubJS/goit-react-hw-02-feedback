ff504a3c89000013893b48f4fdff898024169353
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 02:01:03 +0300

    Update FeedbackOptions.jsx

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -14,3 +14,18 @@ export class FeedbackOptions extends Component {
         )
     }
 }
+
+/*
+FriendList.propTypes = {
+    friends: propTypes.arrayOf(
+        propTypes.shape(
+            {
+                avatar: propTypes.string.isRequired,
+                name: propTypes.string.isRequired,
+                isOnline: propTypes.bool.isRequired,
+                id: propTypes.number.isRequired,
+            }
+        )
+    ).isRequired,
+}
+*/
