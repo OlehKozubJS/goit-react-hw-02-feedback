2a8c0873600a4326fd6bcf962a61be8806c85ce8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 01:47:02 +0300

    Update FeedbackOptions.jsx

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -2,15 +2,15 @@ import { Component } from "react";
 
 export class FeedbackOptions extends Component {
     constructor(props) {
-      super(props);
-      this.state = {optionButtons: this.props.options.map((option, index) => <button className={option.replaceAll(" ", "") + "Button"} key={index}>{option}</button>)};
+        super(props);
+        this.state = {optionButtons: this.props.options.map((option, index) => <button className={option.replaceAll(" ", "") + "Button"} key={index}>{option}</button>)};
     }
   
     render() {
-      return (
-        <div onClick={this.props.onLeaveFeedback}>
-          {this.state.optionButtons}
-        </div>
-      )
+        return (
+            <div onClick={this.props.onLeaveFeedback}>
+                {this.state.optionButtons}
+            </div>
+        )
     }
   }
