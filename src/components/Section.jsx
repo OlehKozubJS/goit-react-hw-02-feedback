033eac1ede16186b53a3972ff5dc43f55a52a966
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-18 19:59:42 +0300

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,7 +6,7 @@ export class Section extends Component {
     neutral: 0,
     bad: 0
   };
-/*
+
   addGood = () => {this.state.good += 1};
   addNeutral = () => {this.addNeutral.neutral += 1};
   addBad = () => {this.addBad.bad += 1};
@@ -20,7 +20,7 @@ export class Section extends Component {
     const sum = this.countTotalFeedback();
     return Math.floor(this.state.good / sum * 100);
   };
-*/
+
   render() {
     const { good, neutral, bad } = this.state;
 
@@ -38,8 +38,8 @@ export class Section extends Component {
                 <li>Good: {good}</li>
                 <li>Neutral: {neutral}</li>
                 <li>Bad: {bad}</li>
-                <li>Total: {this.countTotalFeedback}</li>
-                <li>Positive feedback: {this.countPositiveFeedbackPercentage}%</li>
+                <li>Total: {this.countTotalFeedback()}</li>
+                <li>Positive feedback: {this.countPositiveFeedbackPercentage()}%</li>
             </ul>
         </div>
       </div>
