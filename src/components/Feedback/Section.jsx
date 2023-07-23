f77ceb22288f2a41814b18f5049c5de7965e801a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 05:20:09 +0300

    Update Section.jsx

--- src/components/Feedback/Section.jsx
+++ src/components/Feedback/Section.jsx
@@ -5,8 +5,8 @@ import SectionStyles from "./FeedbackCSS/Section.module.css";
 export class Section extends Component {
     render() {
         return(
-            <div className={SectionStyles.sectionHeader}>
-                <h2>{this.props.title}</h2>
+            <div >
+                <h2 className={SectionStyles.sectionHeader}>{this.props.title}</h2>
                 {this.props.children}
             </div>
         );
