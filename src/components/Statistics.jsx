b37412a33d5e76f974442cd45c03ef56cd05b0de
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 23:55:06 +0300

    Update Statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,21 +1,16 @@
-import { Component } from "react";
 import propTypes from "prop-types";
 import StatisticsStyles from "./FeedbackCSS/Statistics.module.css";
 
-export class Statistics extends Component {
-    render() {
-        const { good, neutral, bad, total, positivePercentage } = this.props;
-
-        return (
-            <ul className={StatisticsStyles.statistics}>
-                <li className={StatisticsStyles.statisticsItem}>Good: {good}</li>
-                <li className={StatisticsStyles.statisticsItem}>Neutral: {neutral}</li>
-                <li className={StatisticsStyles.statisticsItem}>Bad: {bad}</li>
-                <li className={StatisticsStyles.statisticsItem}>Total: {total}</li>
-                <li className={StatisticsStyles.statisticsItem}>Positive feedback: {positivePercentage}%</li>
-            </ul>
-        );
-    }
+export const Statistics = { good, neutral, bad, total, positivePercentage } => {
+    return (
+        <ul className={StatisticsStyles.statistics}>
+            <li className={StatisticsStyles.statisticsItem}>Good: {good}</li>
+            <li className={StatisticsStyles.statisticsItem}>Neutral: {neutral}</li>
+            <li className={StatisticsStyles.statisticsItem}>Bad: {bad}</li>
+            <li className={StatisticsStyles.statisticsItem}>Total: {total}</li>
+            <li className={StatisticsStyles.statisticsItem}>Positive feedback: {positivePercentage}%</li>
+        </ul>
+    );
 }
 
 Statistics.propTypes = {
