73c186763fe7fbcd9397361087555d20fb8d9506
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 23:59:21 +0300

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,11 +1,11 @@
 import propTypes from "prop-types";
 import SectionStyles from "./FeedbackCSS/Section.module.css";
 
-export const Section = ({title}) => {
+export const Section = ({title, children}) => {
     return(
         <div >
             <h2 className={SectionStyles.sectionHeader}>{title}</h2>
-            {this.props.children}
+            {children}
         </div>
     );
 }
