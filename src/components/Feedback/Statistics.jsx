cc05bb51647459a6c739553545fc2c58d5b09fee
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 05:14:00 +0300

    add Statistics styles

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -1,17 +1,18 @@
 import { Component } from "react";
 import propTypes from "prop-types";
+import StatisticsStyles from "./FeedbackCSS/Statistics.module.css";
 
 export class Statistics extends Component {
     render() {
         const { good, neutral, bad, total, positivePercentage } = this.props;
 
         return (
-            <ul>
-                <li>Good: {good}</li>
-                <li>Neutral: {neutral}</li>
-                <li>Bad: {bad}</li>
-                <li>Total: {total}</li>
-                <li>Positive feedback: {positivePercentage}%</li>
+            <ul className={StatisticsStyles.statistics}>
+                <li className={StatisticsStyles.statisticsItem}>Good: {good}</li>
+                <li className={StatisticsStyles.statisticsItem}>Neutral: {neutral}</li>
+                <li className={StatisticsStyles.statisticsItem}>Bad: {bad}</li>
+                <li className={StatisticsStyles.statisticsItem}>Total: {total}</li>
+                <li className={StatisticsStyles.statisticsItem}>Positive feedback: {positivePercentage}%</li>
             </ul>
         );
     }
