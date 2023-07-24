7030a599bc0d84ed9476d89318b72719b784475a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-24 15:53:27 +0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,12 +1,10 @@
-import propTypes from "prop-types";
-import NotificationStyles from "./FeedbackCSS/Notification.module.css"
+import propTypes from 'prop-types';
+import NotificationStyles from './FeedbackCSS/Notification.module.css';
 
-export const Notification = ({message}) => {
-    return(
-        <p className={NotificationStyles.notification}>{message}</p>
-    );
-}
+export const Notification = ({ message }) => {
+  return <p className={NotificationStyles.notification}>{message}</p>;
+};
 
 Notification.propTypes = {
-    message:  propTypes.string.isRequired
-}
+  message: propTypes.string.isRequired,
+};
